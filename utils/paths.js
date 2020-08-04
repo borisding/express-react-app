ac6036c723250053cc40a259412e76b897f91b51
borisding <boris.ding82@outlook.com>
2020-08-04 15:43:51 +0800

    added `paths` util

---
+++ utils/paths.js
@@ -0,0 +1,9 @@
+const root = require('app-root-path');
+
+module.exports = {
+  assets: `${root}/assets`,
+  client: `${root}/client`,
+  public: `${root}/public`,
+  server: `${root}/server`,
+  utils: `${root}/utils`
+};
